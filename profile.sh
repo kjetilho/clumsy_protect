6f7544d68ba086269a53923a4cbdc506b4ff4676
Kjetil Torgrim Homme <kjetil.homme@redpill-linpro.com>
2017-10-31 13:44:57 +0100

    type -p is bash-specific.  only install aliases interactively

--- profile.sh
+++ profile.sh
@@ -1,11 +1,10 @@
 # This file is part of "clumsy_protect"
 # It is intended to be sourced for interactive shells
 
-if type -p clumsy_protect >/dev/null
+if [ "$PS1" != "" ] && type alias clumsy_protect >/dev/null 2>&1
 then
     alias sudo="sudo "
     alias reboot="clumsy_protect reboot"
     alias shutdown="clumsy_protect shutdown"
     alias poweroff="clumsy_protect poweroff"
 fi
-
